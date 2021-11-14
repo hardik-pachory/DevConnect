467a4d129d7d70899438a5e64a7c5963fcd2d638
hardik-pachory <hardik.19jics082@jietjodhpur.ac.in>
2021-11-14 13:19:17 +0530

    JWT Authentication Added

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
+const config = require('config');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const {check, validationResult} = require('express-validator/check')
 
 const User = require('../../models/User');
@@ -25,7 +27,7 @@ router.post('/',
         try {
             // See if user already exists!
             let user = await User.findOne({ email });
-            if (user) {
+            if(user) {
                 return res.status(400).json({errors: [{msg: 'User Already exists'}] });
             }
             
@@ -37,8 +39,18 @@ router.post('/',
             const salt = await bcrypt.genSalt(10);
             user.password = await bcrypt.hash(password, salt);
             await user.save();
+
             // Return jsonwebtoken
-            res.send("User Registered Successfully...!");    
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
+            jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 360000 }, (err, token) => {
+                if (err) throw err;
+                res.json({token});
+            }
+            );
         } catch (err) {
             console.error(err.message);
             res.status(500).send("Server Error");
