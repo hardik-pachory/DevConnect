91daabb8ca04613fd70032dc765af399c5834ba7
hardik-pachory <hardik.19jics082@jietjodhpur.ac.in>
2022-05-20 19:43:56 +0530

    Getting Profile by ID

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -102,4 +102,56 @@ router.post('/',
     }
 );
 
+// @route     GET api/users
+// @desc      Get all profiles
+// @access    Public
+
+router.get('/', async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+        res.json(profiles);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+})
+
+// @route     GET api/profile/user/:user_id
+// @desc      Get profile by User ID
+// @access    Public
+
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate('user', ['name', 'avatar']);
+ 
+    if (!profile) return res.status(400).json({ msg: 'Profile not found' });
+ 
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(400).json({ msg: 'Profile not found' });
+  }
+});
+
+// @route     DELETE api/users
+// @desc      DLETE profile, user, and post
+// @access    Private
+
+router.delete('/', auth, async (req, res) => {
+    try {
+        //  todo - remove users posts
+
+        // Remove profile
+        await Profile.findOneAndRemove({ user: req.user.id });
+        // Remove user 
+        await User.findOneAndRemove({ _id: req.user.id });
+        res.json({ msg: 'Profile and User deleted' });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+})
+
 module.exports = router;
